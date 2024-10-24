edc6dbcbf2e975e4d09fbf42c3bdc16e67210120
monicaoktaviani <116496048+monicaca9@users.noreply.github.com>
2024-10-24 12:23:17 +0700

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -122,6 +122,9 @@ module.exports = {
         },
       ],
     }),
-    new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
   ],
 };
