12fb34d8cd7ba3854405ec83c91fbb0618cc1e55
monicaoktaviani <116496048+monicaca9@users.noreply.github.com>
2024-10-25 08:28:19 +0700

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -21,4 +22,32 @@ module.exports = merge(common, {
       },
     ],
   },
+  plugins: [
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: './sw.bundle.js',
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) => url.href.startsWith('https://restaurant-api.dicoding.dev/list'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'therestodb-api',
+          },
+        },
+        {
+          urlPattern: ({ url }) => url.href.startsWith('https://restaurant-api.dicoding.dev/images/medium/'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'therestodb-api-detail',
+          },
+        },
+        {
+          urlPattern: ({ url }) => url.href.startsWith('https://restaurant-api.dicoding.dev/detail'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'therestodb-api-review',
+          },
+        },
+      ],
+    }),
+  ],
 });
